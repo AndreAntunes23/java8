cc18e9a55323a0a9e7f5c08036ef7727fc2aabbf
AndreAntunes23 <andre_lfa@outlook.com>
2021-10-21 16:44:39 -0300

    Aulas sobre lambda e method references

--- src/OrdenaStrings.java
+++ src/OrdenaStrings.java
@@ -2,51 +2,34 @@ import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class OrdenaStrings {
 
 	public static void main(String[] args) {
-		
+
 		List<String> palavras = new ArrayList<String>();
 		palavras.add("alura online");
 		palavras.add("editora casa do codigo");
 		palavras.add("caelum");
+
+//		palavras.sort((s1, s2) -> Integer.compare(s1.length(), s2.length()));
 		
-		Comparator<String> comparador = new ComparadorPorTamanho();
+		palavras.sort(Comparator.comparing(s -> s.length()));
+		palavras.sort(Comparator.comparing(String :: length));
 		
-//		Collections.sort(palavras, comparador);
+		Function<String, Integer> funcao = String :: length;
+		Function<String, Integer> funcao2 = s -> s.length();
+		
+		Comparator<String> comparador = Comparator.comparing(s -> s.length());
 		palavras.sort(comparador);
-		
+
 		System.out.println(palavras);
 		
-//		antigo
-//		for (String p : palavras) {
-//			System.out.println(p);
-//		}
-		
-		Consumer<String> consumidor = new ImprimeNaLinha();
-		palavras.forEach(consumidor);
+		Consumer<String> impressor = System.out::println;
+		palavras.forEach(impressor);
+
+		palavras.forEach(System.out::println);
+
 	}
-}
-
-class ImprimeNaLinha implements Consumer<String> {
-
-	@Override
-	public void accept(String s) {
-		System.out.println(s);
-	}
-	
-}
-
-class ComparadorPorTamanho implements Comparator<String> {
-
-	@Override
-	public int compare(String s1, String s2) {
-		if(s1.length() < s2.length())
-			return -1;
-		if(s1.length() > s2.length())
-			return 1;
-		return 0;
-	}
-	
 }
